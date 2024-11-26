b2e0d60cdfd323009cf3f3c7683952a08a851245
Luz María <luzmar_80@hotmail.com>
2024-11-25 20:11:35 -0500

    transferencias

--- src/main/java/com/proyecto/ntt/transfer/service/impl/TransferServiceImpl.java
+++ src/main/java/com/proyecto/ntt/transfer/service/impl/TransferServiceImpl.java
@@ -1,19 +1,40 @@
 package com.proyecto.ntt.transfer.service.impl;
 
 import com.proyecto.ntt.transfer.controller.dto.Transfer;
+import com.proyecto.ntt.transfer.repository.TransferRepository;
+import com.proyecto.ntt.transfer.repository.dao.TransferDao;
 import com.proyecto.ntt.transfer.service.TransferService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 @Service
 public class TransferServiceImpl implements TransferService {
+
+    private final TransferRepository repository;
+
+    public TransferServiceImpl(TransferRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<Transfer> transferList() {
-        return List.of();
+        return StreamSupport.stream(repository.findAll().spliterator(), false)
+                .map( transferDao -> {
+                    Transfer transfer = new Transfer();
+                    transfer.setId(transferDao.getId());
+                    transfer.setFecha(transferDao.getFecha());
+                    transfer.setMonto(transferDao.getMonto());
+                    transfer.setNumeroCuentaCliente(transferDao.getNumeroCuentaCliente());
+                    transfer.setNumeroCuentaDestino(transferDao.getNumeroCuentaDestino());
+                    return transfer;
+
+
+                }).toList();
     }
 
     @Override
@@ -23,6 +44,17 @@ public class TransferServiceImpl implements TransferService {
 
     @Override
     public Transfer getTransfer(Integer id) {
+        var transferFound = repository.findById(id);
+        if (transferFound.isPresent()){
+            var transfer = new Transfer();
+            var transferDao = transferFound.get();
+            transfer.setId(transferDao.getId());
+            transfer.setFecha(transferDao.getFecha());
+            transfer.setMonto(transferDao.getMonto());
+            transfer.setNumeroCuentaCliente(transferDao.getNumeroCuentaCliente());
+            transfer.setNumeroCuentaDestino(transferDao.getNumeroCuentaDestino());
+            return transfer;
+        }else {
         return null;
+        }
     }
-}
